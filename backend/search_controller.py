905d3a05e7ad6821d669c51116aec3fa51381234
MarielleVallee <marielle.vallee@gmail.com>
2024-12-13 15:10:28 +0100

    on est mort

--- backend/search_controller.py
+++ backend/search_controller.py
@@ -1,10 +1,25 @@
 from flask import Blueprint, jsonify
 import json
 
+
 search_bp = Blueprint('search_bp', __name__)
 
 #Une route permettant de récupérer de manière paginée
+@search_bp.get('/')
+def get_search():
+    #Idéalement un try catch pour vérifier qu'on envoit bien des nombres au service
+    offset = request.args.get('offset',0)
+    limit = request.args.get('limit',10)
+    int_offset = int(offset)
+    int_limit = int(limit)
 
+    with open('searches.json', 'r') as file:
+        searches_data = json.load(file)
+
+# Appliquer la pagination en utilisant les paramètres offset et limit
+        paginated_data = searches_data[int_offset:int_offset + int_limit]
+
+    return jsonify(paginated_data), 200
 
 #Une route permettant de récupérer toutes les données du fichier `searches.json`, qui sera utilisée pour la visualisation
 
